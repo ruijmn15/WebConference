95f9db59747003527619df64f86d287998ca69d5
Rui Jorge Magalhaes Nunes <a39126@alunos.ipb.pt>
2021-02-20 16:03:58 +0000

    Novas Alterações

--- js/webconf.js
+++ js/webconf.js
@@ -14,8 +14,7 @@ window.onload = function() {
                 const name = document.getElementById('txtName').value
                 const email = document.getElementById('txtEmail').value
                 const url_base = "https://fcawebbook.herokuapp.com"
-                return
-                 fetch (`${url_base}/conference/1/participants/${email}`,{
+                return fetch (`${url_base}/conference/1/participants/${email}`,{
                      headers: {"Content-Type": "apllication/x-www-form-urlencoded" },
                      method: "POST",
                      body: `nomeparticipant=${name}`
@@ -42,3 +41,69 @@ window.onload = function() {
         })
     })
 }
+
+( async () => {
+    const renderSpeakers = document.getElementById("renderSpeakers")
+    let txtSpeakers = ""
+    const response = await fetch (`${urlBase}/conference/1/speakers`)
+    const speakers = await response.json()
+
+    for(const speaker of speakers) {
+        txtSpeakers += `
+        <div class="col-sm-4">
+                <div class="team-member">
+                <img id="${speaker.idSpeaker}" class="mx-auto rounded-circle viewSpeaker" src="${speaker.foto}" alt="">
+                    <h4>${speaker.nome}</h4>
+                    <p class="text-muted">${speaker.cargo}</p>
+                    <ul class="list-inline social-buttons">`
+                        if(speaker.twitter!==null) {
+                            txtSpeakers += `
+                            <li class="list-inline-item">
+                              <a href="${speaker.twitter}" target="_blank">
+                                <i class="fab fa-twitter"></i>
+                              </a>
+                            </li>`
+                        }
+                        if(speaker.facebook!==null) {
+                            txtSpeakers += `
+                            <li class="list-inline-item">
+                              <a href="${speaker.facebook}" target="_blank">
+                                <i class="fab fa-facebook-f"></i>
+                              </a>
+                            </li>`
+                        }
+                        if(speaker.linkedin!==null) {
+                            txtSpeakers += `
+                            <li class="list-inline-item">
+                              <a href="${speaker.linkedin}" target="_blank">
+                                <i class="fab fa-linkedin-in"></i>
+                              </a>
+                            </li>`
+                        }
+                        txtSpeakers += `
+                    </ul>
+                </div>
+            </div>
+            `
+    }
+    renderSpeakers.innerHTML = txtSpeakers
+
+    const btnView = document.getElementById("viewSpeaker")
+    for(let i=0;i < btnView.clientHeight; i++) {
+        btnView[i].addEventListener("click", () => {
+            for(const speaker of speakers) {
+                if(speaker.idSpeaker == btnView[i].getAtribute("id")) {
+                    swal ({
+                        title: speaker.nome,
+                        text: speaker.bio,
+                        imageUrl: speaker.foto,
+                        imageWidth: 400,
+                        imageHeight: 400,
+                        imageAlt: 'Foto do orador',
+                        animation: false
+                    })
+                }
+            }
+        })
+    }
+}) ()
